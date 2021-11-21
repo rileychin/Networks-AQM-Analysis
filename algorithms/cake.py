320ab416ce355fb0a31beb3d102c3116c96195f7
Joon Kang <tnjnkng@gmail.com>
2021-11-22 02:15:24 +0800

    [CAKE] Add cake algorithm and results

---
+++ algorithms/cake.py
@@ -0,0 +1,30 @@
+import os
+
+class CAKE:
+    def __init__(self):
+        self.filename = "cake"
+
+    def router_command(self, router, interface):
+        router.cmd(f'tc qdisc del dev {interface} root')
+        router.cmd(f'tc qdisc add root dev {interface} cake')
+
+
+    def getFileDirectory(self, hostname, protocol):
+        directory = f'./Results/{self.filename}'
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        return f"{directory}/{protocol}_{hostname}.json"
+
+    def udpConnect(self, host, port, target, size):
+        fileDirectory = self.getFileDirectory(host, "udp")
+        print("udp size: %s" % str(size))
+        host.cmd(f'iperf3 -c {target} -u -b {size} -p {port} -J > {fileDirectory}')
+    
+    def tcpConnect(self, host, port, target, size):
+        fileDirectory = self.getFileDirectory(str(host), "tcp")
+        print(f'FileDirectory is : {fileDirectory}')
+        host.cmd(f'iperf3 -c {target} -b {size} -p {port} -J > {fileDirectory}')
+        print(f'iperf3 done on {str(host)}')
+    
+    def __str__(self):
+        return self.filename
